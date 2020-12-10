98e0087cf6461ad9ca1ce8ecc6d66f3f7dd0a180
Nicholas Lai <nicholas_lai_1@hotmail.com>
2020-12-10 00:04:03 -0800

    Final HTML, CSS AND JS changes

--- Final-startingcode/script/script.js
+++ Final-startingcode/script/script.js
@@ -1,79 +1,58 @@
 //Nicholas Lai - ACIT 1620 - script.js
 
-function imageloop(){
-    var gallery = document.getElementById("Thumbnail")
-    for (i = 0; i < imgArray.length; i++) {
-        gallery.appendChild(imgArray[i])
-    }
-};
+// function imageloop(){
+//     var gallery = document.getElementById("Thumbnail")
+//     for (i = 0; i < imgArray.length; i++) {
+//         gallery.appendChild(imgArray[i])
+//     }
+// };
 
-var imgArray = [
+// var imgArray = [
 
-    {image: "one"},
-    {image: "two"},
-    {image: "three"},
-    {image: "four"},
-    {image: "five"}, 
-];
+//     {image: "one"},
+//     {image: "two"},
+//     {image: "three"},
+//     {image: "four"},
+//     {image: "five"}, 
+// ];
 
 
 document.getElementById("bottom").style.visibility = "hidden"
    
 var hoveredimg = document.querySelector(".hoveredimage");
 var displayedimg= document.querySelector(".displayed-img");
-Array.from(document.querySelectorAll(".one")).forEach(item => {
-item.addEventListener("mouseover", event => {
-document.getElementById("bottom").style.visibility = "visible";
-hoveredimg.style.display = "block";
-displayedimg.src = event.target.src;});
-});
-Array.from(document.querySelectorAll(".two")).forEach(item => {
-item.addEventListener("mouseover", event => {
-document.getElementById("bottom").style.visibility = "visible";
-hoveredimg.style.display = "block";
-displayedimg.src = event.target.src;});
-});
-Array.from(document.querySelectorAll(".three")).forEach(item => {
-item.addEventListener("mouseover", event => {
-document.getElementById("bottom").style.visibility = "visible";
-hoveredimg.style.display = "block";
-displayedimg.src = event.target.src;});
-});
-Array.from(document.querySelectorAll(".four")).forEach(item => {
-item.addEventListener("mouseover", event => {
-document.getElementById("bottom").style.visibility = "visible";
-hoveredimg.style.display = "block";
-displayedimg.src = event.target.src;});
-});
-Array.from(document.querySelectorAll(".five")).forEach(item => {
+Array.from(document.querySelectorAll(".img")).forEach(item => {
 item.addEventListener("mouseover", event => {
 document.getElementById("bottom").style.visibility = "visible";
 hoveredimg.style.display = "block";
 displayedimg.src = event.target.src;});
 });
 
+let submit_btn = document.getElementById('submit')
+submit_btn.addEventListener('click', submit)
 
-let new_btn = document.getElementById('submit')
-new_btn.addEventListener('click', submit) 
 
-function submit(e){
+function submit(){
     var x = document.getElementById("effect").value;
         if(x == "blur"){
-        document.getElementById("effect").value = "";
-        if(e.target.id=='one'){
-            document.getElementById("one").src = "images/pic1B.jpg";
-        }
-        if(e.target.id =='two'){
-            document.getElementById("two").src = "images/pic2B.jpg";
-        }
-        if(e.target.id =='three'){
-            document.getElementById("three").src = "images/pic3B.jpg";
-        }
-        if(e.target.id =='four'){
-            document.getElementById("four").src = "images/pic4B.jpg";
-        }
-        if(e.target.id =='five'){
-            document.getElementById("five").src = "images/pic5B.jpg";
-        }
-    }
-}
+            document.getElementById("effect").value = "";
+            if(displayedimg.src = "images/pic1.jpg"){
+                displayedimg.src = "images/pic1B.jpg";
+            }
+            if(displayedimg.src = "images/pic2.jpg"){
+                displayedimg.src = "images/pic2B.jpg";
+            }
+            if(displayedimg.src = "images/pic3.jpg"){
+                displayedimg.src = "images/pic3B.jpg";
+            }
+            if(displayedimg.src = "images/pic4.jpg"){
+                displayedimg.src = "images/pic4B.jpg";
+            }
+            if(displayedimg.src = "images/pic5.jpg"){
+                displayedimg.src = "images/pic5B.jpg";
+            }
+        } 
+        else{
+            alert("Error, Please try again.");
+        }     
+}       
